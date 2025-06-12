f45427a0f68a097fe923fcba458d7c22f2ca0ec7
Berkayy19 <berkay.cinoglu1999@gmail.com>
2025-06-12 09:14:15 +0200

    Lend API hinzugefügt

--- src/block5/aufgabe5.js
+++ src/block5/aufgabe5.js
@@ -1,10 +1,11 @@
 import express, {response} from 'express';
+import { randomUUID } from 'node:crypto';
 import * as fs from "node:fs";
 const app = express();
 const port = 3000;
 app.use(express.json());
 
-const books = [
+let books = [
     {
         "isbn": "978-3-16-148410-0",
         "title": "Die Entdeckung der Langsamkeit",
@@ -18,6 +19,15 @@ const books = [
         "author": "Frank Schätzing"
     }]
 
+let lends = [
+    {
+    "id": "f3d4b6e8-2c9e-4e0c-85a1-b24a2b9b6e7e",
+    "customer_id": "12345",
+    "isbn": "a89de472-70f4-4f9a-9b7a-fcb176c772db",
+    "borrowed_at": "2025-06-12T10:45:00.123Z",
+    "returned_at": null
+    }
+]
 app.get('/books', (req, res) => {
     res.send(books);
 })
@@ -93,6 +103,69 @@ app.patch('/books/:isbn', (req, res) => {
     }
     res.json(book)
 })
+
+app.get('/lends', (req, res) => res.send(
+    res.json(lends)
+))
+
+app.get('/lends/:id', (req, res) => {
+    const { id } = req.params;
+    const lend = lends.find(l => l.id === id);
+    if (!lend) {
+        res.status(404).send("Lend not found");
+    } else{
+        res.send(lend);
+    }
+})
+
+app.post('/lends', (req, res) => {
+    const { customer_id, borrowed_at } = req.body;
+
+    const returned_at = "Not returned yet"
+    let id = randomUUID();
+    while (true) {
+        const found = lends.find(l => l.id === id);
+        if (!found) break;
+        id = randomUUID();
+    }
+
+    let isbn = randomUUID();
+    while (true) {
+        const found = lends.find(l => l.isbn === isbn);
+        if (!found) break;
+        isbn = randomUUID();
+    }
+
+    const lend = {
+        id,
+        customer_id,
+        isbn,
+        borrowed_at,
+        returned_at
+    }
+    lends.push(lend);
+    res.send(lends);
+})
+
+app.delete('/lends/:id', (req, res) => {
+    const { id } = req.params;
+
+    const lend = lends.find(l => l.id === id);
+
+    if (!lend) {
+        return res.status(404).send({ error: 'Lend not found' });
+    }
+
+    if (lend.returned_at) {
+        return res.status(400).send({ error: 'Book already returned' });
+    }
+
+    lend.returned_at = new Date().toISOString();
+
+    res.send(lend);
+});
+
+
 
 app.listen(port, () => {
     console.log(`Server läuft auf Port ${port}`);
